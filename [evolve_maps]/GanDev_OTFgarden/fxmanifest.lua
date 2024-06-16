f073f4fedc2af3709f300ea6304af8ae9a42efe5
VeinDevTtv <142545968+VeinDevTtv@users.noreply.github.com>
2024-06-16 15:40:11 -0700

    2 gang maps + liquor + paleto changes

---
+++ [evolve_maps]/GanDev_OTFgarden/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version "cerulean"
+
+game "gta5"
+
+this_is_a_map 'yes'
+
+lua54 "yes"
+
+file "sp_manifest.ymt"
+
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
+
+
+files {
+	'events.meta',
+	'relationships.dat'
+}
+
+data_file 'FIVEM_LOVES_YOU_4B38E96CC036038F' 'events.meta'
+
+
+client_script 'client.lua'
+
