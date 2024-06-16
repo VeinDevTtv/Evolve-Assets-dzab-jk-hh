1cc079e77fe04a79851df9c1e487b11ee640fa19
VeinDevTtv <142545968+VeinDevTtv@users.noreply.github.com>
2024-06-16 15:47:32 -0700

    eup start

---
+++ [evolve_eup]/evolve_full_eup/fxmanifest.lua
@@ -0,0 +1,28 @@
+-- Generated with AltTool
+
+fx_version 'bodacious'
+game { 'gta5' }
+
+files {
+  'mp_f_freemode_01_mp_f_safr.meta',
+  'mp_f_freemode_01_mp_f_sast.meta',
+  'mp_f_freemode_01_mp_f_vests.meta',
+  'mp_m_freemode_01_mp_m_pants.meta',
+  'mp_m_freemode_01_mp_m_safr.meta',
+  'mp_m_freemode_01_mp_m_sast.meta',
+  'mp_m_freemode_01_mp_m_vests.meta',
+  'mp_m_freemode_01_mp_m_zashoes.meta',
+  'mp_m_freemode_01_swat.meta',
+  'mp_m_freemode_01_zagang.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_zashoes.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_safr.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_sast.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_vests.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_pants.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_safr.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_sast.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_vests.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_zashoes.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_zagang.meta'
